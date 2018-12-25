69669f9992e6397f2c8d386ee5caa048e010b84d
Wenbin Zhu <mighty_zhu@hotmail.com>
2018-12-25 23:24:38 +0800

    add zuul

---
+++ api-gateway/src/main/java/com/mighty/springcloud/zuul/filter/AccessFilter.java
@@ -0,0 +1,55 @@
+package com.mighty.springcloud.zuul.filter;
+
+import com.netflix.ribbon.proxy.annotation.Http;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Access Filter
+ *
+ * @author mighty
+ * @create 2018-12-25 20:19
+ */
+public class AccessFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(AccessFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info("send {} request to {}", request.getMethod(), request.getRequestURL().toString());
+
+        Object accessToken = request.getParameter("accessToken");
+        if (accessToken == null) {
+            log.warn("access token is emtpy.");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            return null;
+        }
+        log.info("access token is ok");
+        return null;
+
+    }
+}
